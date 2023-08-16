c9786030d66976c127f199158da088501d1b4dc8
SebastinGoh <gmcsebastin@gmail.com>
2023-08-16 17:24:43 +0800

    add logic and formatting for 16 squares etch and sketch

--- etch/main.js
+++ etch/main.js
@@ -1,1 +1,13 @@
-console.log("Hellos")
+document.body.onload = makeBoardSquares();
+
+function makeBoardSquares() {
+    for (let times = 0; times < 16; times++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        square.setAttribute("id", times);
+        square.addEventListener('mouseover', function handleClick(event) {
+            square.style.backgroundColor = 'black';
+          });
+        document.getElementById("board").appendChild(square);
+    }
+}
